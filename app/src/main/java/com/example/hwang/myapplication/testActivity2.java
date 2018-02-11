b1fb2cdee342af78013306af5b0e761674493ed2
jihey <zhfwlgp@naver.com>
2018-02-11 16:05:32 +0900

    테스트 추가

--- app/src/main/java/com/example/hwang/myapplication/testActivity2.java
+++ app/src/main/java/com/example/hwang/myapplication/testActivity2.java
@@ -1,8 +1,15 @@
 package com.example.hwang.myapplication;
 
+import android.util.Log;
+
 /**
  * Created by hwang on 2018-02-08.
  */
 
 public class testActivity2 {
+
+
+    public testActivity2() {
+        Log.i("log","test");
+    }
 }
